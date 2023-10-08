034edc5c6e14115bffba69f01766c6c462139529
fatim <fatimamughal103"gmail.com>
2023-10-08 15:09:48 +0500

    loginfirebase

--- fypprojectp/lib/screens/SignIn.dart
+++ fypprojectp/lib/screens/SignIn.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'SignUp.dart';
 import 'home.dart';
@@ -31,223 +32,239 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: const Color(0xffDDE6ED),
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 40,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "SMART",
-                    style: GoogleFonts.inter(
-                      textStyle: const TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.w800,
-                        color: Colors.black,
+      child: WillPopScope(
+        onWillPop: () async {
+          SystemNavigator.pop();
+          return true;
+        },
+        child: Scaffold(
+          backgroundColor: const Color(0xffDDE6ED),
+          body: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "SMART",
+                      style: GoogleFonts.inter(
+                        textStyle: const TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "HELMET",
+                      style: GoogleFonts.inter(
+                        textStyle: const TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.w800,
+                          color: Color(0xffc780ff),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          top: 10.0, left: 35.0, right: 0, bottom: 8.0),
+                      child: Column(
+                        children: [
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text(
+                              "Welcome",
+                              style: GoogleFonts.jomhuria(
+                                textStyle: const TextStyle(
+                                  fontSize: 67,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color(0xff6617ff),
+                                  shadows: [
+                                    Shadow(
+                                      color: Colors.black26,
+                                      offset: Offset(1, 6),
+                                      blurRadius: 2,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      left: 35.0,
+                      top: 0.0 + 50,
+                      child: Text(
+                        "Back",
+                        style: GoogleFonts.jomhuria(
+                          textStyle: const TextStyle(
+                            fontSize: 67,
+                            fontWeight: FontWeight.w400,
+                            color: Color(0xff6617ff),
+                            shadows: [
+                              Shadow(
+                                color: Colors.black26,
+                                offset: Offset(1, 6),
+                                blurRadius: 2,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 61,
+                ),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        controller: emailController,
+                        decoration: const InputDecoration(
+                          hintText: 'Email',
+                          prefixIcon: Icon(Icons.mail_outline_rounded),
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return '\aKindly Enter Valid mail';
+                          }
+                          if (!value.endsWith('@gmail.com')) {
+                            return 'Kindly Enter Valid mail';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 22),
+                      TextFormField(
+                        keyboardType: TextInputType.visiblePassword,
+                        controller: passwordController,
+                        decoration: const InputDecoration(
+                          hintText: 'Password',
+                          prefixIcon: Icon(Icons.lock_outline),
+                        ),
+                        obscureText: true,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Enter Password';
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 70),
+                  child: SizedBox(
+                    height: 40,
+                    width: 140,
+                    child: InkWell(
+                      // onTap: onTap(){},
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Color(0xffc780ff),
+                          // Set the button color to purple
+                        ),
+                        child: Text(
+                          "Log in",
+                          style: GoogleFonts.inter(
+                            textStyle: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xffdde6ed),
+                            ),
+                          ),
+                        ),
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => Home(),
+                              ),
+                            );
+                          } else {}
+                        },
                       ),
                     ),
                   ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    "HELMET",
-                    style: GoogleFonts.inter(
-                      textStyle: const TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.w800,
-                        color: Color(0xffc780ff),
-                      ),
+                ),
+                const SizedBox(
+                  width: 10,
+                  height: 20,
+                ),
+                Text(
+                  'Forget password?',
+                  style: GoogleFonts.inter(
+                    textStyle: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w800,
+                      color: Color(0xff9d6bff),
                     ),
                   ),
-                ],
-              ),
-              Stack(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        top: 10.0, left: 35.0, right: 0, bottom: 8.0),
-                    child: Column(
+                ),
+                const SizedBox(
+                  width: 50,
+                  height: 155,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
                       children: [
-                        Align(
-                          alignment: Alignment.bottomLeft,
+                        Text(
+                          "Don't have an account? ",
+                          style: GoogleFonts.inter(
+                            textStyle: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xff6617ff),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => SignUp(),
+                              ), // Replace SignIn with the actual name of your sign-in screen widget
+                            );
+                          },
                           child: Text(
-                            "Welcome",
-                            style: GoogleFonts.jomhuria(
+                            'Sign up',
+                            style: GoogleFonts.inter(
                               textStyle: const TextStyle(
-                                fontSize: 67,
-                                fontWeight: FontWeight.w400,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w800,
                                 color: Color(0xff6617ff),
-                                shadows: [
-                                  Shadow(
-                                    color: Colors.black26,
-                                    offset: Offset(1, 6),
-                                    blurRadius: 2,
-                                  ),
-                                ],
                               ),
                             ),
                           ),
                         ),
                       ],
                     ),
-                  ),
-                  Positioned(
-                    left: 35.0,
-                    top: 0.0 + 50,
-                    child: Text(
-                      "Back",
-                      style: GoogleFonts.jomhuria(
-                        textStyle: const TextStyle(
-                          fontSize: 67,
-                          fontWeight: FontWeight.w400,
-                          color: Color(0xff6617ff),
-                          shadows: [
-                            Shadow(
-                              color: Colors.black26,
-                              offset: Offset(1, 6),
-                              blurRadius: 2,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 61,
-              ),
-              Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    TextFormField(
-                      controller: emailController,
-                      decoration: const InputDecoration(
-                        hintText: 'Email',
-                        prefixIcon: Icon(Icons.mail_outline_rounded),
-                      ),
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Enter Email';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 22),
-                    TextFormField(
-                      controller: passwordController,
-                      decoration: const InputDecoration(
-                        hintText: 'Password',
-                        prefixIcon: Icon(Icons.lock_outline),
-                      ),
-                      obscureText: true,
-                      validator: (value) {
-                        if (value!.isEmpty) {
-                          return 'Enter Password';
-                        }
-                        return null;
-                      },
-                    ),
                   ],
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 70),
-                child: SizedBox(
-                  height: 40,
-                  width: 140,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xffc780ff),
-                      // Set the button color to purple
-                    ),
-                    child: Text(
-                      "Log in",
-                      style: GoogleFonts.inter(
-                        textStyle: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          color: Color(0xffdde6ed),
-                        ),
-                      ),
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => Home(),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ),
-              const SizedBox(
-                width: 10,
-                height: 20,
-              ),
-              Text(
-                'Forget password?',
-                style: GoogleFonts.inter(
-                  textStyle: const TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w800,
-                    color: Color(0xff9d6bff),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                width: 50,
-                height: 155,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        "Don't have an account? ",
-                        style: GoogleFonts.inter(
-                          textStyle: const TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w400,
-                            color: Color(0xff6617ff),
-                          ),
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => SignUp(),
-                            ), // Replace SignIn with the actual name of your sign-in screen widget
-                          );
-                        },
-                        child: Text(
-                          'Sign up',
-                          style: GoogleFonts.inter(
-                            textStyle: const TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w800,
-                              color: Color(0xff6617ff),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
