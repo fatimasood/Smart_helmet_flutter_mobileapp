7a9ef43f0cab566c19a06523ccee6632397c268c
fatim <fatimamughal103"gmail.com>
2023-10-24 11:16:22 +0500

    addData

--- fypprojectp/lib/screens/SignsScreen.dart
+++ fypprojectp/lib/screens/SignsScreen.dart
@@ -10,6 +10,54 @@ class SignsScreen extends StatefulWidget {
 class _SignsScreenState extends State<SignsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+        child: Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 20, left: 15),
+            child: Row(
+              children: [
+                Container(
+                  width: 120,
+                  height: 120,
+                  child: Image.asset('lib/assets/pic1.png'),
+                ),
+                Container(
+                  width: 120,
+                  height: 120,
+                  child: Image.asset('lib/assets/pic2.png'),
+                ),
+                Container(
+                  width: 120,
+                  height: 120,
+                  child: Image.asset('lib/assets/pic3.png'),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Container(
+                width: 120,
+                height: 120,
+                child: Image.asset('lib/assets/pic4.png'),
+              ),
+              Container(
+                width: 120,
+                height: 120,
+                child: Image.asset('lib/assets/pic5.png'),
+              ),
+              Container(
+                width: 120,
+                height: 120,
+                child: Image.asset('lib/assets/pic6.png'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ));
   }
 }
