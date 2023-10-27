c866ed3a5df84f3160e4bd8e4c313e26c73e55c7
fatim <fatimamughal103"gmail.com>
2023-10-27 14:13:59 +0500

    data

--- fypprojectp/lib/screens/SignsScreen.dart
+++ fypprojectp/lib/screens/SignsScreen.dart
@@ -11,84 +11,85 @@ class _SignsScreenState extends State<SignsScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(top: 30, left: 15),
-            child: Row(
-              children: [
-                Container(
-                  width: 120,
-                  height: 120,
-                  child: Image.asset('lib/assets/pic1.png'),
-                ),
-                Container(
-                  width: 120,
-                  height: 120,
-                  child: Image.asset('lib/assets/pic2.png'),
-                ),
-                Container(
-                  width: 100,
-                  height: 120,
-                  child: Image.asset('lib/assets/pic3.png'),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20),
-            child: Row(
-              children: [
-                Container(
-                  width: 120,
-                  height: 120,
-                  child: Image.asset('lib/assets/pic4.png'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 7),
-                  child: Container(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 30, left: 15),
+              child: Row(
+                children: [
+                  Container(
                     width: 120,
                     height: 120,
-                    child: Image.asset('lib/assets/pic5.png'),
+                    child: Image.asset('lib/assets/pic1.png'),
                   ),
-                ),
-                Container(
-                  width: 108,
-                  height: 120,
-                  child: Image.asset('lib/assets/pic6.png'),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 20),
-            child: Row(
-              children: [
-                Container(
-                  width: 120,
-                  height: 120,
-                  child: Image.asset('lib/assets/pic7.png'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 7),
-                  child: Container(
+                  Container(
                     width: 120,
                     height: 120,
-                    child: Image.asset('lib/assets/pic8.png'),
+                    child: Image.asset('lib/assets/pic2.png'),
                   ),
-                ),
-                Container(
-                  width: 108,
-                  height: 120,
-                  child: Image.asset('lib/assets/pic9.png'),
-                ),
-              ],
+                  Container(
+                    width: 100,
+                    height: 120,
+                    child: Image.asset('lib/assets/pic3.png'),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Row(
+                children: [
+                  Container(
+                    width: 120,
+                    height: 120,
+                    child: Image.asset('lib/assets/pic4.png'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 7),
+                    child: Container(
+                      width: 120,
+                      height: 120,
+                      child: Image.asset('lib/assets/pic5.png'),
+                    ),
+                  ),
+                  Container(
+                    width: 108,
+                    height: 120,
+                    child: Image.asset('lib/assets/pic6.png'),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Row(
+                children: [
+                  Container(
+                    width: 120,
+                    height: 120,
+                    child: Image.asset('lib/assets/pic7.png'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 7),
+                    child: Container(
+                      width: 120,
+                      height: 120,
+                      child: Image.asset('lib/assets/pic8.png'),
+                    ),
+                  ),
+                  Container(
+                    width: 108,
+                    height: 120,
+                    child: Image.asset('lib/assets/pic9.png'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
