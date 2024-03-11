e089e103c02f2a5c71b84a352f958bc9659e05b4
fatim <fatimamughal103"gmail.com>
2024-03-11 23:34:14 +0500

    update home

--- fypprojectp/lib/main.dart
+++ fypprojectp/lib/main.dart
@@ -1,11 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:fypprojectp/screens/Authentication/SignIn.dart';
 import 'package:fypprojectp/screens/SplashScreens.dart';
 import 'package:fypprojectp/screens/Sqflite/DatabaseHelper.dart';
-import 'package:fypprojectp/screens/home.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,23 +9,13 @@ void main() async {
 
   DatabaseHelper databaseHelper = DatabaseHelper(); //initialize Database sql
   await databaseHelper.initializeDatabase();
-  final savedUserId = await getSavedUserId() ?? '';
-  runApp(MyApp(savedUserId: savedUserId));
-}
-
-Future<void> persistLogin(String userId) async {
-  final prefs = await SharedPreferences.getInstance();
-  prefs.setString('userId', userId);
-}
-
-Future<String?> getSavedUserId() async {
-  final prefs = await SharedPreferences.getInstance();
-  return prefs.getString('userId');
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final String? savedUserId;
-  MyApp({required this.savedUserId});
+  const MyApp({super.key});
+
+  // This widget is the root of application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -39,21 +25,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      //home: const SplashScreen(title: 'Smart Helmet'),
-      home: FutureBuilder(
-        future: FirebaseAuth.instance.authStateChanges().first,
-        builder: (context, AsyncSnapshot<User?> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return SplashScreen(title: 'Smart Helmet');
-          } else {
-            if (snapshot.hasData || savedUserId != null) {
-              return Home();
-            } else {
-              return SignIn();
-            }
-          }
-        },
-      ),
+      //home: const EditInformation(),
+      home: const SplashScreen(title: 'Smart Helmet'),
     );
   }
 }
